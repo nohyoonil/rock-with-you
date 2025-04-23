70f2a49afb88861b5ff11f5b0f6b11945c1e02f1
yoon <yunil9631@naver.com>
2025-04-23 19:30:57 +0900

    member entity 생성

---
+++ src/main/java/com/example/rock/entity/Member.java
@@ -0,0 +1,32 @@
+package com.example.rock.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, name = "name")
+    private String name;
+    @Column(nullable = true, name = "pfp")
+    private String pfp;
+    @Column(nullable = false, name = "points")
+    private int points;
+    @Column(nullable = false, name = "vote_sum")
+    private int voteSum;
+    @Column(nullable = false, name = "voted_sum")
+    private int votedSum;
+    @Column(nullable = false, name = "created_at")
+    private LocalDateTime createdAt;
+}
