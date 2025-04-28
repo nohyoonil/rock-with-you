182ab8ed043dd4228db999f213df66904815b992
yoon <yunil9631@naver.com>
2025-04-28 19:00:36 +0900

    kakao 연동 확인

--- src/main/java/com/example/rock/config/SecurityConfig.java
+++ src/main/java/com/example/rock/config/SecurityConfig.java
@@ -2,13 +2,12 @@ package com.example.rock.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +20,10 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/**").authenticated())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(oauth2 ->
+                        oauth2.defaultSuccessUrl("/"));
 
         return http.build();
     }
---
+++ src/main/java/com/example/rock/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.example.rock.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Value("${kakao.auth.client}")
+    private String client;
+    @Value("${kakao.auth.redirect}")
+    private String redirect;
+
+    @GetMapping("/auth/login/kakao")
+    public String kakaoLogin(@RequestParam("code") String accessCode) {
+        return "kakao";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "hello";
+    }
+}
